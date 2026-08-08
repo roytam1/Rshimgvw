0cb63afe8ea6c9e3aa256ce3762dacbe24e8b73a
roytam1 <roytam@gmail.com>
2026-08-08 11:29:44 +0800

    sync with https://github.com/reactos/reactos/commit/a318b6228a24cda9b752501470e61550e925f657

--- shimgvw.h
+++ shimgvw.h
@@ -12,6 +12,7 @@
 #define _INC_WINDOWS
 #define COM_NO_WINDOWS_H
 #define INITGUID
+#define COBJMACROS
 
 #include <windef.h>
 #include <winbase.h>
@@ -19,21 +20,17 @@
 #include <winreg.h>
 #include <wingdi.h>
 #include <wincon.h>
-#ifdef __RSHIMGVW__
-#include <winuser.h>
-#include <winnetwk.h>
-#endif
 #include <objbase.h>
 #include <gdiplus.h>
 #include <shlwapi.h>
 #include <strsafe.h>
-#ifdef __RSHIMGVW__
-typedef IStream* (WINAPI *FN_SHCreateMemStream)(const BYTE *lpbData, UINT dwDataLen);
-#endif
+#include <shobjidl.h>
 
 #include <debug.h>
 
 #include "resource.h"
+
+#define WM_UPDATECOMMANDSTATE (WM_APP + 0)
 
 extern HINSTANCE g_hInstance;
 extern GpImage *g_pImage;
@@ -76,6 +73,12 @@ void Anime_Start(PANIME pAnime, DWORD dwDelay);
 void Anime_Pause(PANIME pAnime);
 BOOL Anime_OnTimer(PANIME pAnime, WPARAM wParam);
 
+void DoShellContextMenuOnFile(HWND hwnd, PCWSTR File, LPARAM lParam);
+void EnableCommandIfVerbExists(UINT ImageId, HWND hwnd, UINT CmdId, PCWSTR Verb, PCWSTR File);
+void ShellExecuteVerb(HWND hwnd, PCWSTR Verb, PCWSTR File, BOOL Quit);
+UINT ErrorBox(HWND hwnd, UINT Error);
+void DisplayHelp(HWND hwnd);
+
 static inline LPVOID QuickAlloc(SIZE_T cbSize, BOOL bZero)
 {
     return HeapAlloc(GetProcessHeap(), (bZero ? HEAP_ZERO_MEMORY : 0), cbSize);
@@ -84,4 +87,47 @@ static inline LPVOID QuickAlloc(SIZE_T cbSize, BOOL bZero)
 static inline VOID QuickFree(LPVOID ptr)
 {
     HeapFree(GetProcessHeap(), 0, ptr);
+}
+
+static inline WORD Swap16(WORD v)
+{
+    return MAKEWORD(HIBYTE(v), LOBYTE(v));
+}
+
+static inline UINT Swap32(UINT v)
+{
+    return MAKELONG(Swap16(HIWORD(v)), Swap16(LOWORD(v)));
+}
+
+#ifdef _WIN32
+#define BigToHost32 Swap32
+#endif
+
+static inline ULARGE_INTEGER MakeULargeInteger(UINT64 value)
+{
+    ULARGE_INTEGER ret;
+    ret.QuadPart = value;
+    return ret;
+}
+
+static inline HRESULT SHIMGVW_HResultFromWin32(DWORD hr)
+{
+     // HRESULT_FROM_WIN32 will evaluate its parameter twice, this function will not.
+    return HRESULT_FROM_WIN32(hr);
+}
+
+static inline HRESULT HResultFromGdiplus(Status status)
+{
+    switch ((UINT)status)
+    {
+        case Ok: return S_OK;
+        case InvalidParameter: return E_INVALIDARG;
+        case OutOfMemory: return E_OUTOFMEMORY;
+        case NotImplemented: return HRESULT_FROM_WIN32(ERROR_CALL_NOT_IMPLEMENTED);
+        case Win32Error: return SHIMGVW_HResultFromWin32(GetLastError());
+        case FileNotFound: return HRESULT_FROM_WIN32(ERROR_FILE_NOT_FOUND);
+        case AccessDenied: return HRESULT_FROM_WIN32(ERROR_ACCESS_DENIED);
+        case UnknownImageFormat: return HRESULT_FROM_WIN32(ERROR_BAD_FORMAT);
+    }
+    return E_FAIL;
 }
